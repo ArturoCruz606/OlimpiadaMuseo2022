d5cc0f565a31cb05d7162f170267b0082275d874
luchoxx87 <ar7ur0321@gmail.com>
2022-09-15 15:07:29 -0300

    Correccion de MVC

--- src/OlimpiadaMuseo2022.Core/Olimpiada2022.Mvc/Models/ErrorViewModel.cs
+++
@@ -1,8 +0,0 @@
-namespace Olimpiada2022.Mvc.Models;
-
-public class ErrorViewModel
-{
-    public string? RequestId { get; set; }
-
-    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-}
